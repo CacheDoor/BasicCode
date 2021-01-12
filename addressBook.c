7287616da9b170a03e2d49163aa5e341568ce8b5
Jeffrey Wang <122535234@qq.com>
2021-01-12 09:12:46 +0800

    2020-01-12

--- addressBook.c
+++ addressBook.c
@@ -3,10 +3,13 @@
 * @Date:   2021-01-11 13:16:00
 * @Email:  wpgraceii@163.com
 * @Last Modified by:   Jeffery Wang
-* @Last Modified time: 2021-01-11 14:45:31
+* @Last Modified time: 2021-01-12 09:07:24
 * @Description: 
 */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 40
 //通讯录结构体
 typedef struct 
 {
@@ -14,35 +17,85 @@ typedef struct
 	char corp[50]; //工作单位
 	char phone[15]; //联系手机
 	char address[100]; //家庭地址
-	char qq[15]; //QQ
+	char QQ[15]; //QQ
 	char mail[50];//邮箱
 }addressInfo; 
 
+int addNewAddressInfo(addressInfo f[]); //增加联系人
+int selectMenu(); //菜单选择
+void outputAddressInfo(addressInfo ai); //单独输出一个人的信息
+int findAddressinfo(addressInfo f[],int n ,char* username); //查找联系人
+int deleteAddressInfo(addressInfo f[],int n); //删除联系人
+void saveAddressInfo(addressInfo f[], int n); //保存联系人
+void searchAddressInfo(addressInfo f[],int n); //查找联系人
+int readAddressInfo(addressInfo f[]); //从文件中读取联系人
+void displayAllAddressInfo(addressInfo f[],int n);//输出指定数量的全部联系人
 
+int main(int argc, char const *argv[])
+{
+	addressInfo ai[MAX];
+	int countPerson = 0;
+	do {
+		switch(selectMenu()){
+			case 1:
+			addNewAddressInfo(ai);
+			break;
+		case 2:
+			deleteAddressInfo(ai,countPerson);
+			break;
+		case 3:
+			searchAddressInfo(ai,countPerson);
+			break;
+		case 4:
+			insertAddressInfo(ai,countPerson);
+			break;
+		case 5:
+			saveAddressInfo(ai,countPerson);
+		 	break;
+		case 6:
+			readAddressInfo(ai);
+			break;
+		case 7:
+			displayAllAddressInfo(ai,countPerson);
+			break;
+		case 8:
+			printf("\n");
+			printf("感谢使用\n");
+			break;
+		default:
+			printf("\n");
+			printf("请输入正确的操作！\n");
+			exit(0);
+			break;
+		}
+	}while(1);
+	return 0;
+}
 //菜单选择
 int selectMenu(){ 
 	char s[80];
-	int c = 0;
+	int c;
 	system("cls");
 	printf("\n");
-	printf("/************************************\\\n")
-	printf("*                                     *\n");
-	printf("*     通 讯 录 管 理 系 统             *\n");
-	printf("*     主      菜       单             *\n");
-	printf("*     0.增加联系人信息                 *\n");
-	printf("*     1.删除联系人信息                 *\n");
-	printf("*     2.查找联系人信息                 *\n");
-	printf("*     3.插入联系人信息                 *\n");
-	printf("*     4.保存联系人信息                 *\n");
-	printf("*     5.从文件加载联系人信息            *\n");
-	printf("*     6.显示所有联系人信息              *\n");
-	printf("*     7.退出管理系统                    *\n");
+	printf("/************************************\\\n");
+	printf("*                                      *\n");
+	printf("*     通 讯 录 管 理 系 统              *\n");
+	printf("*     主      菜       单              *\n");
+	printf("*     1.增加联系人信息                  *\n");
+	printf("*     2.删除联系人信息                  *\n");
+	printf("*     3.查找联系人信息                  *\n");
+	printf("*     4.插入联系人信息                  *\n");
+	printf("*     5.保存联系人信息                  *\n");
+	printf("*     6.从文件加载联系人信息             *\n");
+	printf("*     7.显示所有联系人信息               *\n");
+	printf("*     8.退出管理系统                     *\n");
 	printf("/************************************\\\n");
 	do{
-		printf("请输入对应的序号(0 ~ 7):\n");
-		scanf("%d",s);
-		c = aoti(s); //将字符串转换成整型
-	}while(c < 0 || c > 7);
+		printf("请输入对应的序号(1 ~ 8):\n");
+		scanf("%s",s);
+		c = atoi(s); //将字符串转换成整型
+	}while(c < 0 || c > 8);
+
 	return c;
 }
 
@@ -52,8 +105,8 @@ int addNewAddressInfo(addressInfo f[]){
 	int i,n = 0;
 	char *s = 0;
 	system("cls"); //clear screen
-	printf("请输入添加联系人总人数\n");
-	scanf("%d",n);
+	printf("请输入添加联系人总人数：\n");
+	scanf("%d",&n);
 	printf("请输入记录:\n");
 	printf("姓名   单位   电话   住址   QQ   邮件\n");
 	printf("--------------------------------------\n");
@@ -98,7 +151,7 @@ int deleteAddressInfo(addressInfo f[],int countPerson){
 //通过姓名找到用户的信息
 void searchAddressInfo(addressInfo f[],int countPerson){
 	int i;
-	char username[];
+	char username[50];
 	printf("请输入要查询的人的名字:\n");
 	scanf("%s",username);
 	i = findAddressinfo(f,countPerson,username);
@@ -109,20 +162,37 @@ void searchAddressInfo(addressInfo f[],int countPerson){
 	}
 }
 
+//查找姓名 如何找到返回索引值
+int findAddressinfo(addressInfo f[],int countPerson, char* username){
+	int i = 0;
+	countPerson = sizeof(f) /sizeof(addressInfo);
+	if(0 == strlen(username)){
+		return 0;
+	}
+
+	for (int i = 0; i < countPerson; ++i)
+	{
+		if(username == f[i].name){
+			return i;
+		}
+	}
+	return 0;
+}
+
 //输出用户的信息
 void outputAddressInfo(addressInfo f){
 	system("cls");
-	printf("\n\n*********************************\n");
-	printf("姓名   工作单位   联系电话  住址   QQ   邮件\n");
-	printf("--------------------------------------------");
+	printf("\n\n********************用户信息展示********************************************\n");
+	printf("姓名   工作单位   联系电话     住址       QQ       邮件\n");
+	printf("------------------------------------------------------------------------\n");
 	printf("%-6s%-10s%-10s%-10s%-10s%-10s\n", f.name,f.corp,f.phone,f.address,f.QQ,f.mail);
-	printf("--------------------------------------------");
+	printf("------------------------------------------------------------------------\n");
 	printf("按任意键继续！\n");
 	getch();
 }
 
 //插入联系人的信息
-int insertAddressInfo(addressInfo f,int countPerson){
+int insertAddressInfo(addressInfo f[],int countPerson){
 	int i,j;
 	char username[50]; //在哪个联系人前面插入信息
 	addressInfo tmpAi; //新建一个零时的结构体用户保存插入用户的信息
@@ -197,18 +267,4 @@ void displayAllAddressInfo(addressInfo f[],int countPerson){
 		outputAddressInfo(f[i]);
 		printf("\r\n");
 	}
-}
-
-int main(int argc, char const *argv[])
-{
-	int c = 0;
-	c = selectMenu();
-	switch(c){
-		case 0:
-			addNewAddressInfo();
-			break;
-		case 1:
-			deleteAddressInfo()
-	}
-	return 0;
 }
